f530c1173e41f24d974ec981834a6115dc84cef3
lucasf-nl <180476097+lucasf-nl@users.noreply.github.com>
2024-12-18 14:49:13 +0100

    Add screenshotting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use headless_chrome::Browser;
+use std::{env, fs};
+use headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption;
+
 fn main() {
-    println!("Hello, world!");
+    let browser = Browser::default()
+        .expect("Failed to initialize browser");
+
+    let tab = browser.new_tab()
+        .expect("Failed to open tab");
+
+    tab.navigate_to(&*env::var("WSA_URL").unwrap())
+        .expect("Failed to navigate to URL");
+
+    let png = tab.capture_screenshot(
+        CaptureScreenshotFormatOption::Png,
+        None,
+        None,
+        true
+    ).expect("Failed to capture png");
+    fs::write("screenshot.png", png)
+        .expect("Failed to write to screenshot");
 }
